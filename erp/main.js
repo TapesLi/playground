1175ccf2c991f1e61ce4045060062a2c3a9d6748
tapesli <tapesli@outlook.com>
2016-09-28 00:03:28 +0800

    step 1

--- erp/main.js
+++ erp/main.js
@@ -4,51 +4,66 @@
 
 "use strict";
 
-const fs               = require('fs');
-const parse            = require('csv-parse');
-const transform        = require('stream-transform');
-const stringify        = require('csv-stringify');
-const path             = require('path');
-const iconv            = require('iconv-lite');
-const _                = require('underscore');
+const fs = require('fs');
+const parse = require('csv-parse');
+const transform = require('stream-transform');
+const stringify = require('csv-stringify');
+const path = require('path');
+const iconv = require('iconv-lite');
+const _ = require('underscore');
 const defaultProcessor = function (record, index) {
     return record[index];
 };
 
 const Alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const processors       = {
+const Numbe = [ 0,   1,   2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+function parseNum(numStr) {
+    return parseInt(numStr.split(',').join(''), 10);
+}
+
+const processors = {
     0: function (record, index) {
 
         let cell = record[index];
 
         return cell.split(' ')[0];
     },
-    20: function (record, index) {
+    22: function (record, index) {
 
-        if (parseInt(record[9], 10) + parseInt(record[12], 10) == 0 ) {
-            return
+        if (parseNum(record[13]) + parseNum(record[16]) == 0) {
+            return parseNum(record[12]);
+        } else {
+            if (record[8].trim() == 'Amazon') {
+                return parseNum(record[12]) + parseNum(record[16]);
+            } else {
+                return parseNum(record[12]) + parseNum(record[13]) + parseNum(record[16]);
+            }
         }
 
-
-        if (record[7] == 'Amazon') {
-
-        }
-
-        return index;
     },
+    23: function (record, index) {
 
-    21: function (record, index) {
-        return index;
+        if (parseNum(record[13]) + parseNum(record[16]) == 0) {
+            return parseNum(record[17]) + parseNum(record[18]) + parseNum(record[20]);
+        } else {
+            if (record[8].trim() == 'Amazon') {
+                return parseNum(record[17]) + parseNum(record[18]) + parseNum(record[13]) + parseNum(record[20]);
+            } else {
+                return parseNum(record[17]) + parseNum(record[18]) + parseNum(record[20]);
+            }
+        }
+
     }
 };
 
-let parser      = parse({delimiter: ','});
-let input       = fs.createReadStream(path.join(__dirname, '20168.csv'));
-let transformer = transform(function (record) {
+let parser = parse({delimiter: ','});
+let input = fs.createReadStream(path.join(__dirname, '20168.csv'));
+let transformerStep1 = transform(function (record) {
 
     let result = [];
 
-    _.times(22, (index) => {
+    _.times(24, (index) => {
         let processor = processors[index] || defaultProcessor;
 
         result[index] = processor(record, index);
@@ -60,4 +75,9 @@ let transformer = transform(function (record) {
 let stringifier = stringify();
 
 
-input.pipe(iconv.decodeStream('Shift_JIS')).pipe(parser).pipe(transformer).pipe(stringifier).pipe(process.stdout);
+input
+    .pipe(iconv.decodeStream('Shift_JIS'))
+    .pipe(parser)
+    .pipe(transformerStep1)
+    .pipe(stringifier)
+    .pipe(process.stdout);
