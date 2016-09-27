2958337406cecc7bb5708cc713d9d6b471c670c8
tapesli <tapesli@outlook.com>
2016-09-27 22:00:37 +0800

    hello bottle

---
+++ hello-bottle/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bottle import route, run, template
+
+
+@route('/hello/<name>')
+def index(name):
+    return template('<h1>Hello {{ name }}</h1>', name=name)
+
+
+run(host='localhost', port=8080)
+
